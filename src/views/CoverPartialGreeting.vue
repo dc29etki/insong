3fe57faf2d2b4dddee419e26fe3629a3b03ceded
David Etkin <davidetkin@davidetkin.com>
2020-09-24 17:05:32 -0400

    spacer inserts

--- src/views/CoverPartialGreeting.vue
+++ src/views/CoverPartialGreeting.vue
@@ -11,6 +11,7 @@
 
 <template>
   <div id="order" class="has-footer has-header">
+    <div class="pb-5 mb-5"> </div>
     
     <div class="home-area m-2 text-center" style="margin-bottom: 100px !important;">    
     <h2 class="text-center">Cover Song (Partial)</h2>
