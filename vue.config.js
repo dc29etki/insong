a20483169dfd466728aaf92044e17431f617dd33
Harsukhbir <sagardhiman5@gmail.com>
2020-10-24 22:42:23 +0530

    compatible for IOS and Android

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
 module.exports = {
   runtimeCompiler: true,
+ 
   devServer: {
+      https: false,
       proxy: {
         "/api": {
           target: "localhost"
         }
       }
-    }
+    },
+
+  publicPath: '',
+
+  pluginOptions: {
+    cordovaPath: 'src-cordova'
+  }
 }
